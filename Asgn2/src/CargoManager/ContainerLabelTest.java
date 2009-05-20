15bae931d272e264670932c659f00cadfaf46cf1
Bodaniel Jeanes <me@bjeanes.com>
2009-05-20 12:07:09 +1000

    Cleaned up imports

--- Asgn2/src/CargoManager/ContainerLabelTest.java
+++ Asgn2/src/CargoManager/ContainerLabelTest.java
@@ -1,8 +1,10 @@
 package CargoManager;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
 import org.junit.Before;
 import org.junit.Test;
-import static org.junit.Assert.*;
 
 /**
  * @author Jacob Evans, Bodaniel Jeanes
