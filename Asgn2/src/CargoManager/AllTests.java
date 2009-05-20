e9d98064a001f4221af532bfa0c60d98c3bfb52f
Bodaniel Jeanes <me@bjeanes.com>
2009-05-20 13:03:51 +1000

    Added a testsuite to run all tests at once

---
+++ Asgn2/src/CargoManager/AllTests.java
@@ -0,0 +1,11 @@
+package CargoManager;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses( { ContainerLabelTest.class, CargoInventoryTest.class })
+public class AllTests {
+	//	empty class just to run all of the tests in
+}
