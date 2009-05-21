55ea50225c43f1e3ac4c68b49616d5dce497da46
Jacob Evans <dekz@dekz.net>
2009-05-21 20:26:14 +1000

    fixed isOnBoad

--- Asgn2/src/solution/CargoInventory.java
+++ Asgn2/src/solution/CargoInventory.java
@@ -6,134 +6,139 @@ import java.util.ArrayList;
  * @author Jacob Evans, Bodaniel Jeanes
  */
 public class CargoInventory {
-    int                                  numStacks, maxHeight, maxContainers,
-            currentContainers;
-    ArrayList<ArrayList<ContainerLabel>> storage = new ArrayList<ArrayList<ContainerLabel>>();
+	int numStacks, maxHeight, maxContainers, currentContainers;
+	ArrayList<ArrayList<ContainerLabel>> storage = new ArrayList<ArrayList<ContainerLabel>>();
 
-    public CargoInventory(Integer numStacks, Integer maxHeight,
-            Integer maxContainers) throws CargoException,
-            IllegalArgumentException {
-    
-    	
-        // No null parameters supported
-        if ((numStacks == null) || (maxHeight == null)
-                || (maxContainers == null)) {
-            throw new IllegalArgumentException(
-                    "Cannot have null values for any parameter");
-        }
+	public CargoInventory(Integer numStacks, Integer maxHeight,
+			Integer maxContainers) throws CargoException,
+			IllegalArgumentException {
 
-        // No negative numbers supported
-        if ((numStacks < 0) || (maxHeight < 0) || (maxContainers < 0)) {
-            throw new CargoException("Cannot have negative values");
-        }
+		// No null parameters supported
+		if ((numStacks == null) || (maxHeight == null)
+				|| (maxContainers == null)) {
+			throw new IllegalArgumentException(
+					"Cannot have null values for any parameter");
+		}
 
-        
-        this.numStacks = numStacks;
-        this.maxHeight = maxHeight;
-        this.maxContainers = maxContainers;
-        currentContainers = 0;
+		// No negative numbers supported
+		if ((numStacks < 0) || (maxHeight < 0) || (maxContainers < 0)) {
+			throw new CargoException("Cannot have negative values");
+		}
 
-        for (int i = 0; i < numStacks; i++) {
-            storage.add(new ArrayList<ContainerLabel>());
-        }
-    }
+		this.numStacks = numStacks;
+		this.maxHeight = maxHeight;
+		this.maxContainers = maxContainers;
+		currentContainers = 0;
 
-    public boolean isAccessible(ContainerLabel queryContainer)
-            throws IllegalArgumentException {
-        ContainerLabel topContainer;
-        // may get a null ?
-        if (queryContainer == null) {
-            throw new IllegalArgumentException();
-        }
+		for (int i = 0; i < numStacks; i++) {
+			storage.add(new ArrayList<ContainerLabel>());
+		}
+	}
 
-        try {
-            final ArrayList<ContainerLabel> stack = storage.get(queryContainer
-                    .getKind());
-            topContainer = stack.get(stack.size() - 1);
+	public boolean isAccessible(ContainerLabel queryContainer)
+			throws IllegalArgumentException {
+		ContainerLabel topContainer;
+		// may get a null ?
+		if (queryContainer == null) {
+			throw new IllegalArgumentException();
+		}
 
-            if (topContainer.equals(queryContainer)) {
-                return true;
-            }
-        } catch (final IndexOutOfBoundsException e) {}
+		try {
+			final ArrayList<ContainerLabel> stack = storage.get(queryContainer
+					.getKind());
+			topContainer = stack.get(stack.size() - 1);
 
-        return false;
-    }
+			if (topContainer.equals(queryContainer)) {
+				return true;
+			}
+		} catch (final IndexOutOfBoundsException e) {
+		}
 
-    public boolean isOnboard(ContainerLabel queryContainer)
-            throws IllegalArgumentException {
-       if (queryContainer != null) {
-            //go through the array check if we have the item in the list
-    	   return true;
-        } else {
-            throw new IllegalArgumentException();
-        }
-    }
+		return false;
+	}
 
-    public void loadContainer(ContainerLabel newContainer)
-            throws CargoException, IllegalArgumentException {
-        if (newContainer == null) {
-            throw new IllegalArgumentException("You can not load null");
-        }
+	public boolean isOnboard(ContainerLabel queryContainer)
+			throws IllegalArgumentException {
+		if (queryContainer != null) {
+			// go through the array check if we have the item in the list
+			for (ArrayList<ContainerLabel> i : storage) {
+				for (ContainerLabel containerLabel : i) {
+					if (queryContainer.matches(containerLabel)) {
+						return true;
+					}
+				}
+			}
+			return false;
+		} else {
+			throw new IllegalArgumentException();
+		}
+	}
 
-        if (newContainer.getKind() >= numStacks) {
-            throw new CargoException(
-                    "Container kind can not be stacked on this ship");
-        }
+	public void loadContainer(ContainerLabel newContainer)
+			throws CargoException, IllegalArgumentException {
+		if (newContainer == null) {
+			throw new IllegalArgumentException("You can not load null");
+		}
 
-        if (isOnboard(newContainer)) {
-            throw new CargoException("Container is already on board");
-        }
+		if (newContainer.getKind() >= numStacks) {
+			throw new CargoException(
+					"Container kind can not be stacked on this ship");
+		}
 
-        if (currentContainers >= maxContainers) {
-            throw new CargoException(
-                    "The ship is full and can not hold anymore containers");
-        }
+		if (isOnboard(newContainer)) {
+			throw new CargoException("Container is already on board");
+		}
 
-        if (storage.get(newContainer.getKind()).size() >= maxHeight) {
-            throw new CargoException(
-                    "The stack for this container kind is already at the maximum height");
-        }
+		if (currentContainers >= maxContainers) {
+			throw new CargoException(
+					"The ship is full and can not hold anymore containers");
+		}
 
-        storage.get(newContainer.getKind()).add(newContainer);
-        currentContainers++;
-    }
+		if (storage.get(newContainer.getKind()).size() >= maxHeight) {
+			throw new CargoException(
+					"The stack for this container kind is already at the maximum height");
+		}
 
-    public ContainerLabel[] toArray(Integer kind) throws CargoException,
-            IllegalArgumentException {
-        if (kind == null) {
-            throw new IllegalArgumentException();
-        }
+		storage.get(newContainer.getKind()).add(newContainer);
+		currentContainers++;
+	}
 
-        if ((kind < 0) || (kind >= numStacks)) {
-            throw new CargoException("No such stack on this ship");
-        }
+	public ContainerLabel[] toArray(Integer kind) throws CargoException,
+			IllegalArgumentException {
+		if (kind == null) {
+			throw new IllegalArgumentException();
+		}
 
-        final ContainerLabel[] returnArray = new ContainerLabel[maxHeight];
+		if ((kind < 0) || (kind >= numStacks)) {
+			throw new CargoException("No such stack on this ship");
+		}
 
-        if (kind < numStacks) {
-            return storage.get(kind).toArray(returnArray);
-        } else {
-            throw new CargoException("Kind it out of our range");
-        }
-    }
+		final ContainerLabel[] returnArray = new ContainerLabel[maxHeight];
 
-    public void unloadContainer(ContainerLabel oldContainer)
-            throws CargoException, IllegalArgumentException {
-        // check if the cargo is at the top of the stack
-        if (oldContainer == null) {
-            throw new IllegalArgumentException();
-        }
+		if (kind < numStacks) {
+			return storage.get(kind).toArray(returnArray);
+		} else {
+			throw new CargoException("Kind it out of our range");
+		}
+	}
 
-        if (isOnboard(oldContainer)) {
-            if (isAccessible(oldContainer)) {
-                // remove the container
-                storage.get(oldContainer.getKind()).remove(oldContainer);
-                currentContainers--;
-            } else {
-                throw new CargoException("Cannot access the container");
-            }
-        } else {
-            throw new CargoException("Container is not on the ship");
-        }
-    }
+	public void unloadContainer(ContainerLabel oldContainer)
+			throws CargoException, IllegalArgumentException {
+		// check if the cargo is at the top of the stack
+		if (oldContainer == null) {
+			throw new IllegalArgumentException();
+		}
+
+		if (isOnboard(oldContainer)) {
+			if (isAccessible(oldContainer)) {
+				// remove the container
+				storage.get(oldContainer.getKind()).remove(oldContainer);
+				currentContainers--;
+			} else {
+				throw new CargoException("Cannot access the container");
+			}
+		} else {
+			throw new CargoException("Container is not on the ship");
+		}
+	}
 }
