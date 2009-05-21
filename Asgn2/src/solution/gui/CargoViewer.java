08b801c58f5e5823941ec06f3ce45af726eaac14
Jacob Evans <dekz@dekz.net>
2009-05-21 23:46:46 +1000

    almost have side view

--- Asgn2/src/solution/gui/CargoViewer.java
+++ Asgn2/src/solution/gui/CargoViewer.java
@@ -176,42 +176,47 @@ public class CargoViewer {
 		}
 
 		public void draw(ArrayList<ArrayList<String>> passedArray) {
-            clear();
-            ArrayList<ArrayList<String>> drawArray = passedArray;
-            int maxStack = 0;
-            int currentRow = 0;
-            
-            // find the highest stack
-            for (ArrayList<String> arrayList : drawArray) {
-				if (arrayList.size() > maxStack)
-				{
+			clear();
+			ArrayList<ArrayList<String>> drawArray = passedArray;
+			int maxStack = 0;
+			int currentRow = 0;
+
+			// find the highest stack
+			for (ArrayList<String> arrayList : drawArray) {
+				if (arrayList.size() > maxStack) {
 					maxStack = arrayList.size();
 				}
 			}
-            
-            // we are drawing topdown
-            currentRow = maxStack-1;
-            
-            System.out.print(drawArray.get(0).get(0));
-            //draw the top most strings if there are some, then move on through
-           for (int i = maxStack; i == 0; i--) {
-            	
-            	//go through each one and draw the top most
-            	for (int j = 0; j < drawArray.size(); j++) {
-            		//if we have something to draw then draw it
-					if (!(drawArray.get(j).size() < i)) { 
-						write(drawArray.get(i).get(j));
-						
+
+			// we are drawing topdown
+			currentRow = maxStack;
+
+			System.out.print(drawArray.get(0).get(0));
+			// draw the top most strings if there are some, then move on through
+
+			for (int i = currentRow; i >= 0; i--) {
+				// we start at the top row and go down
+				// if the lists size is equal to or greater than the currentRow
+				// then we have something to draw
+
+				for (int j = 0; j < drawArray.size()-1; j++) {
+					if (drawArray.get(j).size() >= i) {
+						// we have something to draw
+						//check out out of bounds
+						write("| ");
+						write(drawArray.get(j).get(i-1));
+						write(" ");
+					} else {
+						// draw spaces
+						write("           ");	
 					}
-            		
-				
-            	}
+				}
+				newLine();
+
 			}
-           
-				
-        	   
-           }
-        
+
+		}
+
 		@SuppressWarnings("finally")
 		public ArrayList<ArrayList<String>> getData() {
 			ArrayList<ArrayList<String>> drawArray = new ArrayList<ArrayList<String>>();
@@ -223,18 +228,18 @@ public class CargoViewer {
 					ArrayList<String> localDump = new ArrayList<String>();
 					localLabels = inventory.toArray(kind);
 
-				/*	for (ContainerLabel containerLabel : localLabels) {
-						if (containerLabel != null) {
-							localDump.add(containerLabel.toString());
-						}
-					}*/
-					
+					/*
+					 * for (ContainerLabel containerLabel : localLabels) { if
+					 * (containerLabel != null) {
+					 * localDump.add(containerLabel.toString()); } }
+					 */
+
 					for (int i = 0; i < localLabels.length; i++) {
 						if (localLabels[i] != null) {
 							localDump.add(localLabels[i].toString());
 						}
 					}
-					
+
 					drawArray.add(localDump);
 					kind++;
 				}
@@ -243,8 +248,7 @@ public class CargoViewer {
 				System.out.println(drawArray.get(0).get(0));
 				draw(drawArray);
 
-			} finally
-			{
+			} finally {
 				return drawArray;
 			}
 		}
